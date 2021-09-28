655d17986956cad07038c092968cfb321d5df125
Jhonatan <jandres.otalvarop@gmail.com>
2021-09-28 09:19:44 -0500

    argumentos-1

---
+++ 18-argumentos.js
@@ -0,0 +1,13 @@
+//Estructura de una función 
+//Función argumentos
+
+var arguments = 10;
+
+function nuevaFuncion(a,b,c,d){
+
+  
+    console.log(arguments, a, b, c, d);
+
+}
+
+nuevaFuncion("Jhonatan", "Andrés", "Otalvaro", "Palma");
