3261db6233635bdd03db34dc4c0e0acaf98052b4
Jhonatan <jandres.otalvarop@gmail.com>
2021-09-29 09:23:02 -0500

    objeto booleano

---
+++ 23-objetoBooleano.js
@@ -0,0 +1,11 @@
+//objeto booleano
+
+//Creación objetos booleanos.
+var a = true;
+var b = false;
+//--------------------------
+
+//Inicialización de un obejto booleano.
+var a = new Boolean();
+
+console.log(a);
