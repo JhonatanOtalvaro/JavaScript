0db65317bef84aaeee15ce6d7c4bf6994c20564b
Jhonatan <jandres.otalvarop@gmail.com>
2021-10-01 14:41:42 -0500

    objeto string

--- 24-objetoString.js
+++ 24-objetoString.js
@@ -2,4 +2,11 @@
 
 var a = new String("Jhonatan");
 
+console.log(a);
+console.log(a.toUpperCase());
+console.log(a.toLowerCase());
 console.log(a["0"]);
+//--------------------------------
+var b = "Otalvaro";
+
+
