854c5928ece4e51711ebb10f3a848ed3ce2e2fc5
Jhonatan <jandres.otalvarop@gmail.com>
2021-10-04 09:28:42 -0500

    Objeto Date

--- 25-objetoDate.js
+++ 25-objetoDate.js
@@ -1,2 +1,31 @@
 var hoy = new Date();
-console.log(Date());
+console.log("Fecha actual:",hoy);
+console.log("Año actual:",hoy.getFullYear());
+console.log("Dia actual:",hoy.getDate());
+console.log("Hora actual:",hoy.getHours());
+console.log("MiliSegundos:", hoy.getMilliseconds());
+console.log("Mes:", hoy.getMonth());
+console.log("Segundos:", hoy.getSeconds());
+console.log("Representación numérica en mili segundos de una fecha:", hoy.getTime());
+
+var miliSegundos = new Date(1);
+console.log(miliSegundos);
+
+//year, month, day, hour, miliSeconds
+var fechaFija = new Date( 2021, 4, 28, 20, 10, 15);
+console.log(fechaFija);
+
+var Dia = new Date(); 
+for(var i = 0; i < 15000; i++){
+
+console.log("Algo...");
+
+}
+
+var noche = new Date();
+//console.log(Dia);
+//console.log(noche);
+
+var duracion = noche.getTime() - Dia.getTime();
+console.log((duracion/1000), "Segundos");
+console.log(duracion, "MiliSegundos");
