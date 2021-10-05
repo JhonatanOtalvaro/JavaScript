27603ba8d7401513b990fe0f75889e2c7aa0d1b9
Jhonatan <jandres.otalvarop@gmail.com>
2021-10-05 18:35:43 -0500

    Punto y coma.

---
+++ 29-puntoYcoma.js
@@ -0,0 +1,25 @@
+//sin ";"
+
+var a = 10
+var b = 20
+var c = 30
+var d = "Jhonatan"
+
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+
+//Para javaScript después del ; es como terminar una instrucción
+//--------------------------------------------------------------
+//Con ";"
+
+function getNumero(){
+
+    return 10;
+
+}
+
+console.log( getNumero() );
+
+//El ";" es para cerrar una instrucción dentro de la semántica de JavaScript.
