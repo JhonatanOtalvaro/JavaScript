a2ac97417d05ab76e94bf549a66fbf37927a3295
Jhonatan <jandres.otalvarop@gmail.com>
2021-10-05 18:59:49 -0500

    manejo de errores

---
+++ 32-manejoErrores.js
@@ -0,0 +1,14 @@
+try{
+
+    var a = 100;
+
+    console.log("El valor de 'a':", a);
+
+}catch(error){
+
+    console.log("Error de catch",error);
+
+}
+
+//si no se utiliza el "catch" tiene que ir el "finally{}"
+
